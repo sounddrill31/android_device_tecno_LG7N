4ffb60a08856133b7d2dda1c0cd1ffa8dd308670
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-17 21:38:04 +0530

    Update and rename omni_generic_arm64.mk to twrp_generic_arm64.mk

--- twrp_generic_arm64.mk
+++ twrp_generic_arm64.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from generic_arm64 device
 $(call inherit-product, device/unknown/generic_arm64/device.mk)
 
 PRODUCT_DEVICE := generic_arm64
-PRODUCT_NAME := omni_generic_arm64
+PRODUCT_NAME := twrp_generic_arm64
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := GSI on ARM64
 PRODUCT_MANUFACTURER := unknown
