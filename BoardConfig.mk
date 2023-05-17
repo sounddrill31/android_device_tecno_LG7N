80d3aa10efa1826fba739bf3e5b55988bcfb9514
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-05-17 22:10:54 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/unknown/generic_arm64
+DEVICE_PATH := device/tecno/pova4
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
